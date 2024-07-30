36be6619fa06ac7ffc74cc3693e9407678c7d796
undecV <undecv@gmail.com>
2024-07-31 01:36:42 +0800

    🌈 Style: Corrected grammar to adapt to older versions.

--- awthemes/__init__.py
+++ awthemes/__init__.py
@@ -5,7 +5,7 @@ A port of [tcl-awthemes] to Python ttk.
 import logging
 from pathlib import Path
 from tkinter import ttk
-from typing import Union
+from typing import List, Optional, Tuple
 
 import tksvg  # type: ignore
 
@@ -18,23 +18,23 @@ AWTHEMES_PATH: Path = Path(__file__).parent / "awthemes-10.4.0"
 
 class AwthemesStyle(ttk.Style):
     """A style class that loads the awthemes themes into Tkinter."""
-    awthemes_scalable: list[str] = [
+    awthemes_scalable: List[str] = [
         "awarc", "awblack", "awclearlooks", "awwinxpblue", "awbreeze",
         "awbreezedark", "awtemplate"
     ]
-    awthemes: list[str] = ["awdark", "awlight"]
+    awthemes: List[str] = ["awdark", "awlight"]
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.tk.eval(f"set dir {AWTHEMES_PATH.as_posix()}")
         self.tk.eval(f"source {AWTHEMES_PATH.as_posix()}/pkgIndex.tcl")
 
-    def theme_names(self) -> tuple[str]:
+    def theme_names(self) -> Tuple[str]:
         return self.tk.splitlist(self.tk.call("ttk::themes"))
 
     def theme_use(  # type: ignore[override]
-            self, themename: Union[str, None] = None
-            ) -> Union[str, None]:
+            self, themename: Optional[str] = None
+            ) -> Optional[str]:
         if themename is None:
             return super().theme_use(themename=None)
         is_tksvg_loaded = getattr(self.master, "_tksvg_loaded", False)
